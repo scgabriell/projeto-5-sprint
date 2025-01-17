00cc9d1a515ffdd9261671cd566a115c88373829
Gabriel Santos <gabrielsantos1302@gmail.com>
2025-01-17 11:06:50 -0300

    Mais correcoes

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Projeto da Sprint 5 Tripleten
 
 Criando nosso App
 
-Primeira versão do nosso app, suas funcionalidades g
+Primeira versão do nosso app, suas funcionalidades 
+
+Link do app no web: [text](https://projeto-5-sprint.onrender.com) 
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import scipy as sy
 
-car_data = pd.read_csv("C:/Users/gabri/OneDrive/Documentos/GitHub/projeto-5-sprint/data/vehicles.csv") 
+car_data = pd.read_csv("data/vehicles.csv") 
 
 st.header("Título do Cabeçalho")
 
